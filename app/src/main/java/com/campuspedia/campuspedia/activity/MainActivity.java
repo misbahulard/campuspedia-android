65b9073a0a8b72d42dddb24abc4a3b6ab5a9019c
Misbahul Ardani <misbahulard@gmail.com>
2017-10-04 15:03:13 +0700

    Refactor directory dan menyiapkan library

--- app/src/main/java/com/campuspedia/campuspedia/activity/MainActivity.java
+++ app/src/main/java/com/campuspedia/campuspedia/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.campuspedia.campuspedia;
+package com.campuspedia.campuspedia.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.campuspedia.campuspedia.R;
 
 public class MainActivity extends AppCompatActivity {
 
