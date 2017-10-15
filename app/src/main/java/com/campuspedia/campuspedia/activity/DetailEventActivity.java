ad4bcc2eb1bd7baa9ec6dd47b45f61deb7d3061c
HP\Nadia Widad <widadnadia@gmail.com>
2017-10-15 11:50:40 +0700

    buat layout detail event

---
+++ app/src/main/java/com/campuspedia/campuspedia/activity/DetailEventActivity.java
@@ -0,0 +1,15 @@
+package com.campuspedia.campuspedia.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.campuspedia.campuspedia.R;
+
+public class DetailEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_event);
+    }
+}
